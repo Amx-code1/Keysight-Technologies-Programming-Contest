5b91e1557a2ea40f2daca5ebd80d5b8d2c436466
Amx-code1 <xtreme.amx.45@gmail.com>
2026-04-19 07:55:20 +0530

    Delete taskB.py

--- taskB.py
+++
@@ -1,8 +0,0 @@
-N, M=map(int, input().split())
-F = list(map(int, input().split()))
-
-# question 1 Are all N people wearing different types of clothes ?
-print("Yes" if len(set(F))==N else "No")
-
-# question 2  For every one of the M types of clothes, is there at least one person wearing that type?
-print("Yes" if len(set(F))==M else "No")
